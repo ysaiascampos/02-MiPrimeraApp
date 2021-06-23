0f7e3864b5eb45272a87da1c55cdc26287c4142a
ysaiascampos <ysaiascampos@gmail.com>
2021-06-22 21:37:50 -0400

    FlexScreen flexdirection

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -9,6 +9,8 @@ export const FlexScreen = () => {
             <Text style={styles.caja1}>Caja 1</Text>
             <Text style={styles.caja2}>Caja 2</Text>
             <Text style={styles.caja3}>Caja 3</Text>
+            <Text style={styles.caja3}>Caja 3</Text>
+            <Text style={styles.caja3}>Caja 3</Text>
         </View>
         
     )
@@ -18,21 +20,20 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#28C4D9',
         flex: 1,
+        flexDirection:'row',
     },
     caja1:{
-        flex: 4,
+        flex: 1,
         borderWidth: 2,
         borderColor: 'white',
         fontSize: 30,
     },
     caja2:{
-        flex: 4,
         borderWidth: 2,
         borderColor: 'white',
         fontSize: 30,
     },
     caja3:{
-        flex: 2,
         borderWidth: 2,
         borderColor: 'white',
         fontSize: 30,
