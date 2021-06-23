76185cf8897da4e4178e438f408bb2a6a5cdee28
ysaiascampos <ysaiascampos@gmail.com>
2021-06-22 21:27:57 -0400

    FlexScreen Flex

---
+++ src/screens/FlexScreen.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+
+
+export const FlexScreen = () => {
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.caja1}>Caja 1</Text>
+            <Text style={styles.caja2}>Caja 2</Text>
+            <Text style={styles.caja3}>Caja 3</Text>
+        </View>
+        
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#28C4D9',
+        flex: 1,
+    },
+    caja1:{
+        flex: 4,
+        borderWidth: 2,
+        borderColor: 'white',
+        fontSize: 30,
+    },
+    caja2:{
+        flex: 4,
+        borderWidth: 2,
+        borderColor: 'white',
+        fontSize: 30,
+    },
+    caja3:{
+        flex: 2,
+        borderWidth: 2,
+        borderColor: 'white',
+        fontSize: 30,
+    }
+});
